c36202f815b6c79e71d30550bad7c3749a304b84
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2025-02-21 11:02:26 -0300

    Remove dead code

--- src/main/java/dev/lmarchesoti/workshopmongo/resources/UserResource.java
+++ src/main/java/dev/lmarchesoti/workshopmongo/resources/UserResource.java
@@ -19,11 +19,6 @@ public class UserResource {
 
     @GetMapping()
     public ResponseEntity<List<User>> findAll() {
-//        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-//        User alex = new User("2", "Alex Green", "alex@gmail.com");
-//
-//        List<User> userList = new ArrayList<>(Arrays.asList(maria, alex));
-
         List<User> userList = userService.findAll();
 
         return ResponseEntity.ok().body(userList);
