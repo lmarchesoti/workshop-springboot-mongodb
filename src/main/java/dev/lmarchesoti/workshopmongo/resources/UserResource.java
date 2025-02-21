ea3e4778a826b02da1c2ef07d8dc62dd75a80739
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2025-02-21 11:50:29 -0300

    Adiciona criação de usuários

--- src/main/java/dev/lmarchesoti/workshopmongo/resources/UserResource.java
+++ src/main/java/dev/lmarchesoti/workshopmongo/resources/UserResource.java
@@ -7,9 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,6 +36,16 @@ public class UserResource {
         UserDTO dto = new UserDTO(userService.findById(id));
 
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
+        User user = userService.fromDto(dto);
+        user = userService.insert(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/dev/lmarchesoti/workshopmongo/services/UserService.java
+++ src/main/java/dev/lmarchesoti/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package dev.lmarchesoti.workshopmongo.services;
 
 import dev.lmarchesoti.workshopmongo.domain.User;
+import dev.lmarchesoti.workshopmongo.dto.UserDTO;
 import dev.lmarchesoti.workshopmongo.repository.UserRepository;
 import dev.lmarchesoti.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,15 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found with id " + id));
     }
 
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDto(UserDTO obj) {
+        User user = new User();
+        user.setId(obj.getId());
+        user.setName(obj.getName());
+        user.setEmail(obj.getEmail());
+        return user;
+    }
 }
