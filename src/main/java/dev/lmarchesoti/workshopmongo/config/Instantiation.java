6b85f641d82ed6a0b5754e915dae6914adb09791
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2025-02-21 11:07:19 -0300

    Adiciona operação de instanciação da base de dados

---
+++ src/main/java/dev/lmarchesoti/workshopmongo/config/Instantiation.java
@@ -0,0 +1,27 @@
+package dev.lmarchesoti.workshopmongo.config;
+
+import dev.lmarchesoti.workshopmongo.domain.User;
+import dev.lmarchesoti.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.deleteAll();
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+
+}
