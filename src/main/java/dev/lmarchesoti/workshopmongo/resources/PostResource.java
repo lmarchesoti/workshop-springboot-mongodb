5b954db2c26d4b127749e623e01112d3bec6a29e
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2025-02-28 11:54:11 -0300

    Consulta posts por titulo contendo texto

--- src/main/java/dev/lmarchesoti/workshopmongo/resources/PostResource.java
+++ src/main/java/dev/lmarchesoti/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package dev.lmarchesoti.workshopmongo.resources;
 
 import dev.lmarchesoti.workshopmongo.domain.Post;
+import dev.lmarchesoti.workshopmongo.resources.util.URL;
 import dev.lmarchesoti.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +24,12 @@ public class PostResource {
     public ResponseEntity<Post> findPost(@PathVariable("id") String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findPostByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        String titlesearch = URL.decodeParam(text);
+        List<Post> posts = postService.findByTitle(titlesearch);
+        return ResponseEntity.ok().body(posts);
     }
 }
--- src/main/java/dev/lmarchesoti/workshopmongo/services/PostService.java
+++ src/main/java/dev/lmarchesoti/workshopmongo/services/PostService.java
@@ -6,6 +6,8 @@ import dev.lmarchesoti.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
@@ -14,5 +16,9 @@ public class PostService {
 
     public Post findById(String id) {
         return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Post not found with id " + id));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
